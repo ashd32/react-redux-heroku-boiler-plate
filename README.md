cec55c16e0bf2ba7c12722eabb80bebdefe2f42e
Guy McManus <guy@guymcmanus.com>
2019-08-23 14:27:20 +1000

    added in the files from the traversy media course, which i will change to update the boilerplate

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # react-redux-heroku-boiler-plate
-boiler plate that includes front end back end, MERN stack with redux and deployment to heroku.  Includes authentication.
+
+boiler plate that includes front end back end, MERN stack with redux and deployment to heroku. Includes authentication.
+
+Steps:
+
+- git clone this repo, or fork it
+- npm i at the root level
+- cd into the client, then npm i
+- to get it working run "npm run dev" from the server at the root level
---
+++ client/src/App.js
@@ -0,0 +1,39 @@
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Routes from './components/routing/Routes';
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <Router>
+        <>
+          <Navbar />
+          <Switch>
+            <Route exact path='/' component={Landing} />
+            <Route component={Routes} />
+          </Switch>
+        </>
+      </Router>
+    </Provider>
+  );
+};
+
+export default App;
+
+//so any route that we want to force the user to be logged in for we can use PrivateRoute
